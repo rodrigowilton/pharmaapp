10b4e615092be7d220ac1888b5ba72ff743f2a9a
rodrigo.muniz <rodrigo.muniz@tatticasegunca.com.br>
2026-03-11 16:25:18 -0300

    feat: PharmaApp inicial ajuste

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://n8n-pharmaapp-api.ngnshu.easypanel.host'  // URL da API
-  // OU
-  // apiUrl: 'http://SEU_IP_VPS:3333'  // IP direto da VPS
+  apiUrl: 'https://n8n-pharmaapp-api.ngnsuh.easypanel.host'
 };
